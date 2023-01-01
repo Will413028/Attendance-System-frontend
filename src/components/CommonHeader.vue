b2b7e51314ba8bc3bd99d2d910d61a03fbf9ef1f
Will <will413028@gmail.com>
2023-01-01 13:28:58 +0800

    feat: add dropdown menu

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -8,8 +8,37 @@
       </el-button>
     </div>
     <div class="r-content"></div>
+    <el-dropdown>
+      <span class="el-dropdown-link">
+        <img class="user" :src="getImgage_src('vue.svg')">
+        <el-icon class="el-icon--right">
+          <arrow-down />
+        </el-icon>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>About</el-dropdown-item>
+          <el-dropdown-item>Logout</el-dropdown-item>
+          <el-dropdown-item disabled>Action 4</el-dropdown-item>
+          <el-dropdown-item divided>Action 5</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
   </el-header>
 </template>
+
+<script>
+export default {
+  setup() {
+    const getImgage_src = (base) =>{
+      return new URL(`../assets/${base}`, import.meta.url).href;
+    };
+    return {
+      getImgage_src
+    }
+  },
+}
+</script>
 
 
 <style lang="less" scoped>
