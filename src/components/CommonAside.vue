e29ee381ecb439570f2d768fbc45c527643ed3bf
Will <will413028@gmail.com>
2023-01-01 13:00:33 +0800

    refactor: modify the aside style

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,6 +1,11 @@
 <template>
   <el-aside width="200px">
-    <el-menu class="el-menu-vertical-demo">
+    <el-menu
+      class="el-menu-vertical-demo"
+      background-color="#545c64"
+      text-color="#fff"
+      :collapse="false"
+    >
       <el-menu-item
         :index="item.path"
         v-for="item in noChildren()"
@@ -9,7 +14,26 @@
         <component class="icons" :is="item.icon"></component>
         <span>{{ item.label }}</span>
       </el-menu-item>
-
+      <el-sub-menu
+        :index="item.label"
+        v-for="item in hasChildren()"
+        :key="item.path"
+      >
+        <template #title>
+          <component class="icons" :is="item.icon"></component>
+          <span>{{ item.label }}</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item
+            :index="subItem.path"
+            v-for="(subItem, subIndex) in item.children"
+            :key="subIndex"
+          >
+            <component class="icons" :is="subItem.icon"></component>
+            <span>{{ subItem.label }}</span>
+          </el-menu-item>
+        </el-menu-item-group>
+      </el-sub-menu>
     </el-menu>
   </el-aside>
 </template>
@@ -21,9 +45,29 @@ export default {
       {
         path: "/user",
         name: "user",
-        label: "user_management",
+        label: "user_name",
         icon: "user",
         url: "UserManage/UserManage",
+      },
+      {
+        label: "other",
+        icon: "location",
+        children: [
+          {
+            path: "/page1",
+            name: "page1",
+            label: "page1",
+            icon: "setting",
+            url: "Other/PageOne",
+          },
+          {
+            path: "/page2",
+            name: "page2",
+            label: "page2",
+            icon: "setting",
+            url: "Other/PageTwo",
+          },
+        ],
       },
     ];
 
@@ -49,5 +93,4 @@ export default {
   width: 18px;
   height: 18px;
 }
-
 </style>
--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -1,14 +1,14 @@
 <template>
-    <el-header>
-        <div class="l-content">
-            <el-button size="small">
-                <el-icon :size="20">
-                    <Menu />
-                </el-icon>
-            </el-button>
-        </div>
-        <div class="r-content"></div>
-    </el-header>
+  <el-header>
+    <div class="l-content">
+      <el-button size="small">
+        <el-icon :size="20">
+          <Menu />
+        </el-icon>
+      </el-button>
+    </div>
+    <div class="r-content"></div>
+  </el-header>
 </template>
 
 
