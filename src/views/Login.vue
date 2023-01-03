523a38367e816bfaa3ab96756f2c2a5243cffed0
Will <will413028@gmail.com>
2023-01-03 15:38:01 +0800

    feat: add Login page

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-form :model="loginForm" class="login-container">
+    <h3>Login</h3>
+    <el-form-item>
+      <el-input
+        type="input"
+        placeholder="account"
+        v-model="loginForm.account"
+      >
+      </el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input
+        type="password"
+        placeholder="password"
+        v-model="loginForm.password"
+      >
+      </el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="login"> Login </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+<script>
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+export default {
+  setup() {
+    const loginForm = reactive({
+      username: "employee1",
+      password: "titaner",
+    });
+
+    const router = useRouter();
+    const login = async () => {
+
+      router.push({
+        name: "home",
+      });
+    };
+    return {
+      loginForm,
+      login,
+    };
+  },
+};
+</script>
+<style lang="less" scoped>
+.login-container {
+  width: 350px;
+  background-color: #fff;
+  border: 1px solid #eaeaea;
+  border-radius: 15px;
+  padding: 35px 35px 15px 35px;
+  box-shadow: 0 0 25px #cacaca;
+  margin: 180px auto;
+  h3 {
+    text-align: center;
+    margin-bottom: 20px;
+    color: #505450;
+  }
+}
+</style>
