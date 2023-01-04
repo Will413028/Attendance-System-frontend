9e9ba03ae0c3fb11cd35f93ac612ec447cab05f6
Will <will413028@gmail.com>
2023-01-04 14:46:20 +0800

    feat: add user & token to localStorge

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,40 @@ import { createStore } from 'vuex';
 
 export default createStore({
     state: {
-        isCollapse: true
+        isCollapse: true,
+        user: {
+            id: 0,
+            name: "",
+            account: "",
+            role: ""
+        },
+        token: ''
     },
     mutations: {
         updateIsCollapse(state, payload) {
             state.isCollapse = !state.isCollapse
+        },
+        setToken(state, val) {
+            state.token = val
+            localStorage.setItem('token', JSON.stringify(val))
+        }, 
+        clearToken(state) {
+            state.token = ''
+            localStorage.remove('token')
+        },
+        getToken(state) {
+            state.token = state.token || localStorage.get('token')
+        },
+        setUser(state, val) {
+            state.user = val
+            localStorage.setItem('user', JSON.stringify(val))
+        }, 
+        clearUser(state) {
+            state.user = ''
+            localStorage.remove('user')
+        },
+        getUser(state) {
+            state.user = state.user || localStorage.get('user')
         }
     }
 });
