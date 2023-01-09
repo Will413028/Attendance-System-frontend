96efc75fe1db288f0c4b5ff3b7eefc5641a27398
Will <will413028@gmail.com>
2023-01-09 09:20:57 +0800

    fix: fix the query parameter of QRcodeURL

--- src/api/QRcode.js
+++ src/api/QRcode.js
@@ -2,7 +2,7 @@ import config from "../config/config";
 
 function getQRcodeURL(user_id, token, latitude, longitude, time) {
     let baseURL = config.baseApi;
-    return `${baseURL}/createAttendanceQRcode?user_id=${user_id}&token=${token}&${latitude}&${longitude}&${time}`;
+    return `${baseURL}/createAttendanceQRcode?user_id=${user_id}&token=${token}&latitude=${latitude}&longitude=${longitude}&timestamp=${time}`;
 }
 
 export default getQRcodeURL
