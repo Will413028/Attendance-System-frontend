5554e32c9800eb535f23af987a644a71cceb6b1f
will <will413028@gmail.com>
2024-01-31 17:03:49 +0800

    feat: use next.js

--- src/api/QRcode.js
+++
@@ -1,8 +0,0 @@
-import config from "../config/config";
-
-function getQRcodeURL(user_id, token, latitude, longitude, time) {
-    let baseURL = config.baseApi;
-    return `${baseURL}/attendanceQRcode?user_id=${user_id}&token=${token}&latitude=${latitude}&longitude=${longitude}&date=${time}`;
-}
-
-export default getQRcodeURL
